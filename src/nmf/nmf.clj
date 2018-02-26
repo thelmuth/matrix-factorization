1ae24437670e14a4ccf2cb66530c4f7e4c3e5687
Tom Helmuth <thelmuth@hamilton.edu>
2018-02-25 21:43:51 -0500

    Added real data from a Replace Space With Newline run. Also, added timing of each step.

--- src/nmf/nmf.clj
+++ src/nmf/nmf.clj
@@ -49,21 +49,25 @@
   "Given non-negative matrix V, returns two matrices W and H such that V = WH
   Implemented using multiplicative update rule (Lee and Seung, 2001)"
   [V k max-iter]
-  (loop [W (rand-matr (height V) k 1)
-         H (rand-matr k (width V) 1)
-         iter max-iter]
-    (println iter " " (diff-cost V (matr-mult W H))) ; DEBUG
-    (if (or (<= iter 0) (= (diff-cost V (matr-mult W H)) 0))
-      [W H]
-      (let [new-H (matr-doall (matr-map * H
-                    (matr-map / (matr-mult (transpose W) V)
-                                (matr-mult (transpose W) W H))))]
-        (recur
-          (matr-doall (matr-map * W
-            (matr-map / (matr-mult V (transpose new-H))
-                        (matr-mult W new-H (transpose new-H)))))
-          new-H
-          (- iter 1))))))
+  (let [start-time (System/currentTimeMillis)]
+    (loop [W (rand-matr (height V) k 1)
+           H (rand-matr k (width V) 1)
+           iter max-iter]
+      (print iter " " (diff-cost V (matr-mult W H))) ; DEBUG
+      (println "\ttime in seconds: " (float (/ (- (System/currentTimeMillis)
+                                                              start-time)
+                                                           1000)))
+      (if (or (<= iter 0) (= (diff-cost V (matr-mult W H)) 0))
+        [W H]
+        (let [new-H (matr-doall (matr-map * H
+                                          (matr-map / (matr-mult (transpose W) V)
+                                                    (matr-mult (transpose W) W H))))]
+          (recur
+           (matr-doall (matr-map * W
+                                 (matr-map / (matr-mult V (transpose new-H))
+                                           (matr-mult W new-H (transpose new-H)))))
+           new-H
+           (- iter 1)))))))
 
 ; (defn nmf
 ;   "Given non-negative matrix V, returns two matrices W and H such that V = WH
